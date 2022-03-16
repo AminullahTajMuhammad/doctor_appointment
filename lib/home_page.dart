8cab6532db7de2cd13df3f09a2521f33c8e2534f
Amin <aminullahtajmuhammad@gmail.com>
2022-03-17 03:40:10 +0500

    Home screen UI Complete.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -28,7 +28,9 @@ class _HomePageState extends State<HomePage> {
       children: [
         buildAppBar(),
         buildGreetings(),
-        buildSearch()
+        buildSearch(),
+        buildCategories(),
+        buildDoctorsList()
       ],
     );
   }
@@ -154,10 +156,209 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget buildCategories() {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 20, top: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Categories",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: GoogleFonts.mulish().fontFamily,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 20),
+                child: Text(
+                  "See All",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                    fontFamily: GoogleFonts.mulish().fontFamily,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 120,
+          margin: const EdgeInsets.only(left: 20, right: 10, top: 10),
+          child: ListView(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            children: [
+              categoryTile("Surgeon", "assets/ic_surgeon.png", true),
+              categoryTile("Urologist", "assets/ic_kidney.png", false),
+              categoryTile("Dentist", "assets/ic_dentist.png", false),
+              categoryTile("Allergist", "assets/ic_allergy.png", false),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget categoryTile(String title, String img, bool isSelected) {
+    return Container(
+      width: 100,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(right: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: isSelected ? Constants.PRIMARY_COLOR : const Color(0xffE3E3E3),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            img,
+            height: 50,
+            width: 50,
+            color: isSelected ? Constants.PRIMARY_COLOR : Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 16.5,
+                fontFamily: GoogleFonts.mulish().fontFamily,
+                color: isSelected ? Constants.PRIMARY_COLOR : Colors.black
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Widget buildDoctorsList() {
-    return Container();
+    return Container(
+      child: ListView(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        children: [
+          doctorListTile("assets/dr_1.png", "Dr. Smith", "Surgeon", "4.7", "10.00 AM - 03.00 AM", true),
+          doctorListTile("assets/dr_2.png", "Dr. Steve Son", "Urologist", "4.5", "10.00 AM - 03.00 AM", false),
+          doctorListTile("assets/dr_3.png", "Dr. Banner", "Dentists", "5.0", "10.00 AM - 03.00 AM", false),
+          doctorListTile("assets/dr_4.png", "Dr. Clart Jon", "Surgeon", "4.1", "10.00 AM - 03.00 AM", false),
+        ],
+      ),
+    );
+  }
+
+  Widget doctorListTile(String img, String name, String specialist, String rating, String time, bool isSelected) {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, top: 15, right: 20),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: const Color(0xffE3E3E3),
+        ),
+        color: isSelected ? Constants.PRIMARY_COLOR : const Color(0xffFFFFFF),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            margin: const EdgeInsets.only(left: 5),
+            child: CircleAvatar(
+              backgroundImage: AssetImage(img),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                        fontSize: 16.5,
+                        fontFamily: GoogleFonts.mulish().fontFamily,
+                        fontWeight: FontWeight.w600,
+                        color: isSelected ? Colors.white : Colors.black
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 2),
+                  child: Text(
+                    specialist,
+                    style: TextStyle(
+                        fontSize: 13,
+                        fontFamily: GoogleFonts.mulish().fontFamily,
+                        color: isSelected ? Colors.white : Colors.black
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset("assets/ic_rating.svg", height: 16,),
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          rating,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: GoogleFonts.mulish().fontFamily,
+                            color: isSelected ? Colors.white : Colors.black
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SvgPicture.asset(
+                              "assets/ic_time.svg", height: 16,
+                              color: isSelected ? Colors.white : const Color(0xff436E8E),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(left: 5),
+                              child: Text(
+                                time,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  fontFamily: GoogleFonts.mulish().fontFamily,
+                                  color: isSelected ? Colors.white : Colors.black
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
